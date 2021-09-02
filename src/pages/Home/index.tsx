d7266ed34df852ca805510cf8dbc428d838caee2
Hitalo Alves <hitalo.ralves@outlook.com>
2021-09-02 20:34:30 -0300

    Adicionando funcionalidades ao header e cart

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -54,7 +54,7 @@ const Home = (): JSX.Element => {
               <li key={product.id}>
               <img src={product.image} alt="Tênis de Caminhada Leve Confortável" />
               <strong>{product.title}</strong>
-              <span>R$ 179,90</span>
+              <span>{formatPrice(product.price)}</span>
               <button
                 type="button"
                 data-testid="add-product-button"
